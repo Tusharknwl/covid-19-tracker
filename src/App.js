d9fc315e99cf84c25853088cc97a6eb9913b50a3
Tushar <>
2021-05-10 21:17:54 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Cards, Charts } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+class App extends React.Component {
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData });
+    }
+    render() {
+        const { data } = this.state;
+
+        return(
+            <div className={styles.container}>
+                <Cards data={data}/>
+                <Charts />
+                
+            </div>
+        )
+    }
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+// import Cards from './components/Cards/Cards';
+// import Charts from './components/Charts/Charts';
+
+
+
+ReactDOM.render(<App />,
+ document.getElementById('root'));
