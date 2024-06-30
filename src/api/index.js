921e6f43cc5624e5f1143b7a50d605d9f148ddfe
Tushar Khanagwal <4653280-Tusharknwl@users.noreply.replit.com>
2024-06-30 14:59:26 +0000

    api updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,33 @@
 import axios from 'axios';
 
-const url = 'https://covid19.mathdro.id/api';
+const url = 'https://disease.sh/v3/covid-19/all';
 
 export const fetchData = async () => {
-    try {
-        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+  try {
+    const { data: { cases, recovered, deaths, updated } } = await axios.get(url);
+    console.log({ cases, recovered, deaths });
 
-        
-        return { confirmed, recovered, deaths, lastUpdate };
-    } catch (error) {
-
-    }
+    return { cases, recovered, deaths, updated };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
 export const fetchDailyData = async () => {
-      try {
-        const { data } = await axios.get(`${url}/daily`);
+  try {
+    const { data } = await axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=all');
+    console.log(data);
 
-        // console.log(data);
-    
-        return data.map(({ confirmed, deaths , reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
-      } catch (error) {
-        return error;
-      }
-    };
+    // Transform the data to the desired format
+    const formattedData = Object.keys(data.cases).map((date) => ({
+      date,
+      confirmed: data.cases[date],
+      deaths: data.deaths[date]
+    }));
 
-  
+    return formattedData;
+  } catch (error) {
+    console.error("Error fetching daily data:", error);
+    return error;
+  }
+};
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,54 +1,51 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
-import CountUp from 'react-countup'
-import cx from 'classnames' 
+import CountUp from 'react-countup';
+import cx from 'classnames';
 
 import styles from './Cards.module.css';
 
-const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
-    if(!confirmed) {
+const Cards = ({ data: { cases, recovered, deaths, updated } }) => {
+    if (!cases) {
         return 'Loading...';
     }
-    
 
-    console.log(deaths.value);
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
                 <Grid item component={Card} xs={9} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
-                    <Typography gutterBottom>Infected</Typography>
-                    <Typography variant="h5" className={styles.inf}>
-                        <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
-                    </Typography>
-                    <Typography >{new Date(lastUpdate).toDateString()}</Typography>
-                    <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                        <Typography gutterBottom>Infected</Typography>
+                        <Typography variant="h5" className={styles.inf}>
+                            <CountUp start={0} end={cases} duration={2.5} separator="," />
+                        </Typography>
+                        <Typography>{new Date(updated).toDateString()}</Typography>
+                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card} xs={9} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
-                    <Typography  gutterBottom>Recovered</Typography>
-                    <Typography variant="h5" className={styles.rec}>
-                        <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
-                    </Typography>
-                    <Typography>{new Date(lastUpdate).toDateString()}</Typography>
-                    <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+                        <Typography gutterBottom>Recovered</Typography>
+                        <Typography variant="h5" className={styles.rec}>
+                            <CountUp start={0} end={recovered} duration={2.5} separator="," />
+                        </Typography>
+                        <Typography>{new Date(updated).toDateString()}</Typography>
+                        <Typography variant="body2">Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card} xs={9} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
-                    <Typography gutterBottom>Deaths</Typography>
-                    <Typography variant="h5" className={styles.dea}>
-                        <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
-                    </Typography>
-                    <Typography>{new Date(lastUpdate).toDateString()}</Typography>
-                    <Typography variant="body2">Number of Deaths cause by COVID-19</Typography>
+                        <Typography gutterBottom>Deaths</Typography>
+                        <Typography variant="h5" className={styles.dea}>
+                            <CountUp start={0} end={deaths} duration={2.5} separator="," />
+                        </Typography>
+                        <Typography>{new Date(updated).toDateString()}</Typography>
+                        <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
         </div>
-    )
-        
-}
+    );
+};
 
 export default Cards;
